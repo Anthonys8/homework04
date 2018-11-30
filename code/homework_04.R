7ef9690169bfdbd2b980d68ccadfcafcbe4868df
Anthonys8 <45113358+Anthonys8@users.noreply.github.com>
2018-11-29 23:13:16 -0800

    -reorganized the file to be put in the right folder -save the data in data folder -save the figures in the figures folder

--- code/homework_04.R
+++ code/homework_04.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readr)
+library(here)
 
 # load the data
 df <- read.csv("raw_data/murders.csv", na.strings = c("", "NA", "#N/A"))
@@ -7,6 +8,10 @@ df <- read.csv("raw_data/murders.csv", na.strings = c("", "NA", "#N/A"))
 # filter the data to year 1995 only and remove na
 murders <- filter(df[complete.cases(df),], year == 1995)
 summary(murders)
+
+# save the data in csv file
+
+write.csv(murders, here("data", "Murders1995.csv"))
 
 # Finding the correlation between variables
 
@@ -31,6 +36,8 @@ murders <- murders %>%
 murders %>% ggplot(aes(x = stand_res)) +
   geom_histogram() + xlab("Standardized residuals")
 
+ggsave(here("figures", "firstModel_standardizedResidual_histogram.png"))
+
 # Residual vs Fitted Value for first model
 
 murders$fitted <- firstModel$fitted.values
@@ -38,6 +45,7 @@ murders$residuals <- firstModel$residuals
 murders %>% ggplot(aes(x = fitted, y = residuals)) +
   geom_point()
 
+ggsave(here("figures", "firstModel_residual_vs_fittedValue.png"))
 
 # Second Model
 secondModel <- lm(murdrate ~ perc1019 + perc2029 + percblack + percmale + rpcpersinc,
@@ -53,6 +61,8 @@ murders <- murders %>%
 murders %>% ggplot(aes(x = stand_res)) +
   geom_histogram() + xlab("Standardized residuals")
 
+ggsave(here("figures", "secondModel_standardizedResidual_histogram.png"))
+
 # Residual vs Fitted Value for second model
 
 murders$fitted <- secondModel$fitted.values
@@ -60,3 +70,4 @@ murders$residuals <- secondModel$residuals
 murders %>% ggplot(aes(x = fitted, y = residuals)) +
   geom_point()
 
+ggsave(here("figures", "secondModel_residual_vs_fittedValue.png"))
