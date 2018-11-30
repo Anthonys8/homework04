7bf88566f2062ac6a531d0420a55638d347e4c86
Anthonys8 <45113358+Anthonys8@users.noreply.github.com>
2018-11-29 22:38:06 -0800

    -upload the data -removing na from the data and filter it to year 1995 data -do summary analysis of the data -check the correlation of the variables -create first model of linear regression -plot the standardized residual histogram, and residual vs fitted value

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# homework04
-homework 4 files and codes
+# Problem set 4
+
+**Purpose: Practice multivariate regression and diagnostics**
+
+## Directory structure
+
+I have already set up the following directories:
+
+- raw_data: original data files.
+- code: data cleaning, variables, and analysis
+- data: derived data files and results
+- figures: generated figures and other figures for paper
+- tables: generated tables
+
+You may decide not to use all of these.
+
+Some directories have a ReadMe file in them, but the
+file contains just the name of the directory.
+The reason is that this ensures that the directory
+gets committed to GitHub.
+
+
---
+++ homework_04.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(readr)
+
+# load the data
+df <- read.csv("raw_data/murders.csv", na.strings = c("", "NA", "#N/A"))
+
+# filter the data to year 1995 only and remove na
+murders <- filter(df[complete.cases(df),], year == 1995)
+summary(murders)
+
+# Finding the correlation between variables
+
+cor(murders$murders, murders$arrests)
+cor(murders$popul, murders$arrests)
+cor(murders$murders, murders$popul)
+cor(murders$murdrate, murders$arrestrate)
+cor(murders$murders, murders$percblack)
+
+# First Model
+
+firstModel <- lm(murders ~ perc1019 + perc2029 + percblack + percmale + rpcpersinc,
+                 data = murders)
+summary(firstModel)
+
+# Standardised residuals plot for first model
+
+murders <- murders %>% 
+  mutate(
+    stand_res = rstandard(firstModel)
+  )
+murders %>% ggplot(aes(x = stand_res)) +
+  geom_histogram() + xlab("Standardized residuals")
+
+# Residual vs Fitted Value for first model
+
+murders$fitted <- firstModel$fitted.values
+murders$residuals <- firstModel$residuals
+murders %>% ggplot(aes(x = fitted, y = residuals)) +
+  geom_point()
